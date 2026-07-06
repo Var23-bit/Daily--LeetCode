d27464171ab71e718efe47ffd03e160694a9a788
Varun Kumar <varunkumar62058@gmail.com>
2026-07-06 20:09:52 +0530

    Time: 79 ms (5.03%) | Memory: 33.8 MB (5.30%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.cpp
+++ 1222-remove-covered-intervals/remove-covered-intervals.cpp
@@ -12,7 +12,7 @@ public:
         sort(intervals.begin(),intervals.end(),lambda);
         result.push_back(intervals[0]);
         for(int i=0;i<n;i++){
-            if(result.back()[0]<=intervals[i][0] && result.back()[1]>=intervals[i][1]){
+            if(result.back()[1]>=intervals[i][1]){
             continue;
             }
             result.push_back(intervals[i]);
